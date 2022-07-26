bd9b47f2ae87b556c800c9da37d8824619f71768
Jaejeong Sohn <jaejlf@naver.com>
2022-07-26 14:37:43 +0900

    ✅ [MOD] Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl ${PROJECT_NAME} | grep jar | awk '{print $1}')
+CURRENT_PID=$(lsof -ti tcp:8080)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
